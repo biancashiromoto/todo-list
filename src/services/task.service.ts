36accd2f77b699f7c437c466d6904691413f870b
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-11-28 11:30:49 -0300

    feat: Add correct implementation for findAll

--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -1,6 +1,18 @@
+import taskController from "../controller/task.controller";
 import { Task } from "../models/Task";
+import { TaskType } from "../types/TaskType"
 
-const findAll = async (): Promise<void> => {
-    console.log("AAAA");
-    console.log(Task);
+type Response = {
+    status: number,
+    data: TaskType[],
+}
+
+const findAll = async (): Promise<Response> => {
+    const tasks = await Task.findAll();
+    const mappedTasks: TaskType[] = tasks.map((task) => task.get({plain: true}));
+    return { status: 200, data: mappedTasks };
+}
+
+export default {
+    findAll
 }
