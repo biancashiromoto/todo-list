2823474719877f5b4aa69ce83edfccedfe23fe0c
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-11-28 12:27:50 -0300

    feat: Create function update

--- src/controller/task.controller.ts
+++ src/controller/task.controller.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
-import taskService from "../services/task.service"
-import { TaskType } from "../types/TaskType";
+import taskService from "../services/task.service";
 
 const findAll = async (req:Request, res: Response) => {
     const { status, data } = await taskService.findAll();
@@ -15,11 +14,18 @@ const findByPk = async (req: Request, res: Response) => {
 
 const register = async (req: Request, res: Response) => {
     const { status, data } = await taskService.register(req.body);
+    res.status(status).json(data);  
+}
+
+const update = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    const { status, data } = await taskService.update(Number(id), req.body);
     res.status(status).json(data);
 }
 
 export default {
     findAll,
     findByPk,
-    register
+    register,
+    update
 }
--- src/services/task.service.ts
+++ src/services/task.service.ts
@@ -9,7 +9,7 @@ const findAll = async (): Promise<Response> => {
     return { status: 200, data: mappedTasks };
 }
 
-const findByPk = async (id: number): Promise<Response> => {
+const findByPk = async (id: TaskType["id"]): Promise<Response> => {
     const task = await Task.findByPk(id);
     if (!task) return { status: 404, data: "Error - There is no task with this id" };
     const mappedTask: TaskType = task.get({plain: true});
@@ -22,8 +22,22 @@ const register = async (data: TaskType): Promise<Response> => {
     return { status: 201, data: mappedTask };
 }
 
+const update = async (id: TaskType["id"], data: TaskType): Promise<Response> => {
+    const task = await Task.findByPk(id);
+    if (!task) return { status: 404, data: "Error - There is no task with this id" };
+    const updatedLines = await Task.update(data, { where: { id } });
+    if (updatedLines[0] !== 0) {
+        const updatedTask = await Task.findByPk(id);
+        const mappedTask: TaskType = updatedTask!.get({plain: true});
+        return { status: 200, data: mappedTask };
+    } else {
+        return { status: 400, data: "There was an error while updating the task. Please try again."};
+    }
+}
+
 export default {
     findAll,
     findByPk,
-    register
+    register,
+    update
 }
