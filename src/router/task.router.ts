8e7f87ca93505bf18fe561a2c424cdf33b2730f1
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-11-28 11:43:23 -0300

    feat: Add route to /tasks/:id

--- src/router/task.router.ts
+++ src/router/task.router.ts
@@ -5,5 +5,6 @@ const express = require("express");
 const taskRouter = express.Router();
 
 taskRouter.get("/", taskController.findAll);
+taskRouter.get("/:id", taskController.findByPk);
 
 export default taskRouter;
