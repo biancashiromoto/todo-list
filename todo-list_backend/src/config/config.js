8eff95b09932db2547297817b5017a8d17513874
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-11-30 17:35:16 -0300

    chore: Update config.js

--- todo-list_backend/src/config/config.js
+++ todo-list_backend/src/config/config.js
@@ -1,6 +1,5 @@
 const config = {
   username: process.env.MYSQL_USER || "root",
-  // password: process.env.MYSQL_ROOT_PASSWORD || "root_password",
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE || "todo_list",
   host: process.env.MYSQL_HOST,
