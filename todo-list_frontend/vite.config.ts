de060c238694b6e0a79480623e53d16c2535b59e
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-11-30 19:00:41 -0300

    build: Update vite.config.js

--- todo-list_frontend/vite.config.ts
+++ todo-list_frontend/vite.config.ts
@@ -3,6 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    cors: false,
+    open: false,
+    host: "0.0.0.0",
+    port: 8080,
+  },
   plugins: [react()],
   build: {
     outDir: 'dist',
