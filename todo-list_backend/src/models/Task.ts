2163ddaa6f726fca95712312f744ba0bae6b90d2
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-11-30 17:00:50 -0300

    chore: Update models

--- todo-list_backend/src/models/Task.ts
+++ todo-list_backend/src/models/Task.ts
@@ -8,8 +8,8 @@ const sequelize: Sequelize = new Sequelize({
     dialect: 'mysql',
     host: "localhost",
     username: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
+    password: process.env.MYSQL_ROOT_PASSWORD,
+    database: process.env.MYSQL_DATABASE || "todo_list",
   });
 
 export const Task = sequelize.define("Task", {
