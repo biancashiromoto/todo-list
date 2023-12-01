5b28b77427e2ef0294bd93ab82ad55f529fa3313
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-11-30 22:07:21 -0300

    feat: Connect to API using axios

--- todo-list_frontend/src/App.tsx
+++ todo-list_frontend/src/App.tsx
@@ -1,7 +1,15 @@
-// import React from "react";
+import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+  
+  useEffect(() => {
+    axios.get("http://localhost:3000/tasks").then(response => {
+      console.log(response.data);
+    })
+  }, []);
 
   return (
     <>
