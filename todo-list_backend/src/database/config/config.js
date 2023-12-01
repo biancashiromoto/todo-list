b22f2b56af41bba45a023a10beb2e933fcf37697
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-01 19:50:44 -0300

    raw: Update database name

--- todo-list_backend/src/database/config/config.js
+++ todo-list_backend/src/database/config/config.js
@@ -14,7 +14,7 @@ dotenv.config();
 const config = {
   username: process.env.MYSQL_USER || "root",
   password: process.env.MYSQL_PASSWORD || "root_password",
-  database: process.env.MYSQL_DATABASE || "database",
+  database: process.env.MYSQL_DATABASE || "todo_list",
   host: process.env.MYSQL_HOST || "localhost",
   dialect: 'mysql',
   logging: false,
