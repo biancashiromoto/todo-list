d4711c22c3f7ca638aef2c054187378b1c14e609
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-01 18:45:58 -0300

    raw: Update config.js

--- todo-list_backend/src/database/config/config.js
+++ todo-list_backend/src/database/config/config.js
@@ -1,12 +1,26 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+// const config = {
+//   username: process.env.MYSQL_USER,
+//   password: process.env.MYSQL_PASSWORD,
+//   database: process.env.MYSQL_DATABASE,
+//   host: process.env.MYSQL_HOST,
+//   dialect: 'mysql',
+//   logging: false,
+// };
+
 const config = {
   username: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE || "todo_list",
-  host: process.env.MYSQL_HOST,
+  password: process.env.MYSQL_PASSWORD || "root_password",
+  database: process.env.MYSQL_DATABASE || "database",
+  host: process.env.MYSQL_HOST || "localhost",
   dialect: 'mysql',
   logging: false,
 };
 
+// console.log(config);
 
 module.exports = {
   development: config,
