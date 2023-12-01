90619ab4e9f2fa7130b16a4d9423f13764b518d7
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-01 13:22:02 -0300

    feat: Fetch data and display in App.tsx

--- todo-list_frontend/src/App.tsx
+++ todo-list_frontend/src/App.tsx
@@ -1,19 +1,34 @@
-import { useEffect } from 'react';
-import './App.css';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { Task } from "./types/Task";
 
 
 function App() {
+  const [data, setData] = useState([]);
   
   useEffect(() => {
     axios.get("http://localhost:3000/tasks").then(response => {
       console.log(response.data);
+      setData(response.data);
     })
   }, []);
 
   return (
     <>
       <h1>Todo list</h1>
+      {data.length > 0 ? (
+        data.map((task: Task) => (
+        <div>
+          <label>
+            <input type="checkbox" />
+            {task.title}
+          </label>
+          <p>Status: {task.status}</p>
+          <p>Priority: {task.priority}</p>
+        </div>
+        ))
+      ) : ""}
     </>
   )
 }
