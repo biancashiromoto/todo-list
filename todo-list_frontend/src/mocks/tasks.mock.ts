9e089dad86183491822f57be08ddec429aef5b00
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-01 13:22:42 -0300

    feat: Create tasks mock

---
+++ todo-list_frontend/src/mocks/tasks.mock.ts
@@ -0,0 +1,16 @@
+import { Task } from "../types/Task";
+
+export const mockTasks: Task[] = [
+    {
+        "id": 1,
+        "title": "Sample task 01",
+        "priority": "medium",
+        "status": "in progress"
+    },
+    {
+        "id": 2,
+        "title": "Sample task 02",
+        "priority": "high",
+        "status": "completed"
+    }
+];
