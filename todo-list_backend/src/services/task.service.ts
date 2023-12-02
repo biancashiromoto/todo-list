6246be9bb34f833c9ca956a31048dd593b2daee4
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-01 21:52:43 -0300

    refactor: Implement OOP in task.service

--- todo-list_backend/src/services/task.service.ts
+++ todo-list_backend/src/services/task.service.ts
@@ -1,48 +1,37 @@
-import { Task } from "../database/models/task.model";
+import { ITask } from "../interfaces/ITask";
+import TaskModel from "../models/task.model";
+import { ResponseError, ResponseErrorType, ResponseSuccess } from "../types/Responses";
 import { TaskType } from "../types/TaskType";
-import { Response } from "../types/Responses";
 
-const findAll = async (): Promise<Response> => {
-    const tasks = await Task.findAll();
-    const mappedTasks: TaskType[] = tasks.map((task) => task.get({plain: true}));
-    return { status: 200, data: mappedTasks };
-}
+export default class TaskService {
+    constructor(
+        private taskModel: ITask = new TaskModel(),
+    ) {}
 
-const findByPk = async (id: TaskType["id"]): Promise<Response> => {
-    const task = await Task.findByPk(id);
-    if (!task) return { status: 404, data: "Error - There is no task with this id" };
-    const mappedTask: TaskType = task.get({plain: true});
-    return { status: 200, data: mappedTask };
-}
-
-const register = async (data: TaskType): Promise<Response> => {
-    const newTask = await Task.create(data);
-    const mappedTask: TaskType = newTask.get({plain: true});
-    return { status: 201, data: mappedTask };
-}
-
-const update = async (id: TaskType["id"], data: TaskType): Promise<Response> => {
-    const task = await Task.findByPk(id);
-    if (!task) return { status: 404, data: "Error - There is no task with this id" };
-    const updatedLines = await Task.update(data, { where: { id } });
-    if (updatedLines[0] !== 0) {
-        const updatedTask = await Task.findByPk(id);
-        const mappedTask: TaskType = updatedTask!.get({plain: true});
-        return { status: 200, data: mappedTask };
-    } else {
-        return { status: 400, data: "There was an error while updating the task. Please try again."};
+    public async findAll(): Promise<ResponseSuccess> {
+        const tasks = await this.taskModel.findAll();
+        return { status: "SUCCESSFUL", data: tasks }
     }
-}
 
-const deleteTask = async (id: TaskType["id"]): Promise<Response> => {
-    await Task.destroy({ where: { id }});
-    return { status: 200, data: `Task with id ${id} successfully deleted.`}
-}
+    public async findByPk(id: TaskType["id"]): Promise<ResponseSuccess | ResponseError> {
+        const task = await this.taskModel.findByPk(id);
+        if (!task) return { status: "NOT_FOUND", data: { message: "Task not found"}};
+        return { status: "SUCCESSFUL", data: task };
+    }
 
-export default {
-    findAll,
-    findByPk,
-    register,
-    update,
-    deleteTask
+    public async register(task: TaskType): Promise<ResponseSuccess> {
+        const newTask = await this.taskModel.register(task);
+        return { status: "SUCCESSFUL", data: newTask }
+    }
+
+    public async update(id: TaskType["id"], task: TaskType): Promise<ResponseSuccess | ResponseError> {
+        const updatedTask = await this.taskModel.update(id, task);
+        if (!updatedTask) return { status: "NOT_FOUND", data: { message: "Task not found" }};
+        return { status: "SUCCESSFUL", data: updatedTask };
+    }
+
+    public async deleteTask(id: TaskType["id"]): Promise<ResponseSuccess | ResponseError> {
+        const message = await this.taskModel.deleteTask(id);
+        return { status: "SUCCESSFUL", data: { message }};
+    }
 }
