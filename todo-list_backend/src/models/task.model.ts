8a618215c52663735570f672ca2267b404b19022
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-01 21:53:05 -0300

    refactor: Implement OOP in task.models

--- todo-list_backend/src/models/task.model.ts
+++ todo-list_backend/src/models/task.model.ts
@@ -10,25 +10,25 @@ export default class TaskModel implements ITask {
         return tasks.map((task) => task.dataValues);
     }
 
-    async findByPk(id: number): Promise<ITask | null> {
+    async findByPk(id: TaskType["id"]): Promise<ITask | null> {
         const task = await this.model.findByPk(id);
         if (!task) return null;
         return task.dataValues;
+    }    
+    
+    async register(task: TaskType): Promise<ITask> {
+        const newTask = await this.model.create(task);
+        return newTask.dataValues;
     }
     
-    async update(id: number, task: TaskType): Promise<ITask | null> {
+    async update(id: TaskType["id"], task: TaskType): Promise<ITask | null> {
         const updatedLines = await this.model.update(task, { where: { id }});
         if (updatedLines[0] === 0) return null;
         const updatedTask = await this.model.findByPk(id);
         return updatedTask?.dataValues;
     }
 
-    async register(task: TaskType): Promise<ITask> {
-        const newTask = await this.model.create(task);
-        return newTask.dataValues;
-    }
-
-    async deleteTask(id: number): Promise<string> {
+    async deleteTask(id: TaskType["id"]): Promise<string> {
         await this.model.destroy({ where: { id }});
         return "Task successfully deleted."
     }
