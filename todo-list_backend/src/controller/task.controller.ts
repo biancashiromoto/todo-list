2e65ea6a808f3be8dce2f6f3e7015c5cf9f3114f
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-01 23:00:26 -0300

    feat: Create instance of TaskService

--- todo-list_backend/src/controller/task.controller.ts
+++ todo-list_backend/src/controller/task.controller.ts
@@ -1,32 +1,34 @@
 import { Request, Response } from "express";
-import taskService from "../services/task.service";
+import TaskService from "../services/task.service";
+
+const taskService: TaskService = new TaskService();
 
 const findAll = async (req:Request, res: Response) => {
     const { status, data } = await taskService.findAll();
-    res.status(status).json(data);
+    res.status(200).json(data);
 }
 
 const findByPk = async (req: Request, res: Response) => {
     const { id } = req.params;
     const { status, data } = await taskService.findByPk(Number(id));
-    res.status(status).json(data);
+    res.status(200).json(data);
 }
 
 const register = async (req: Request, res: Response) => {
     const { status, data } = await taskService.register(req.body);
-    res.status(status).json(data);  
+    res.status(201).json(data);  
 }
 
 const update = async (req: Request, res: Response) => {
     const { id } = req.params;
     const { status, data } = await taskService.update(Number(id), req.body);
-    res.status(status).json(data);
+    res.status(200).json(data);
 }
 
 const deleteTask = async (req: Request, res: Response) => {
     const { id } = req.params;
     const { status, data } = await taskService.deleteTask(Number(id));
-    res.status(status).json(data);
+    res.status(200).json(data);
 }
 
 export default {
