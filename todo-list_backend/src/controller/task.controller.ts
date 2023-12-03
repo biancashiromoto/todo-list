6ed374ee3450f0736ca4844d9d3d7cfceb59ee5a
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-03 16:06:01 -0300

    feat: Implement mapStatusHTTP

--- todo-list_backend/src/controller/task.controller.ts
+++ todo-list_backend/src/controller/task.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import TaskService from "../services/task.service";
+import mapStatusHTTP from "../utils/mapStatusHTTP";
 
 export default class TaskController {
     private taskService: TaskService;
@@ -10,29 +11,29 @@ export default class TaskController {
 
     public findAll = async (req: Request, res: Response) => {
         const { status, data } = await this.taskService.findAll();
-        res.status(200).json(data);
+        res.status(mapStatusHTTP(status)).json(data);
     }
 
     public findByPk = async (req: Request, res: Response) => {
         const { id } = req.params;
         const { status, data } = await this.taskService.findByPk(Number(id));
-        res.status(200).json(data);
+        res.status(mapStatusHTTP(status)).json(data);
     }
 
     public register = async (req: Request, res: Response) => {
         const { status, data } = await this.taskService.register(req.body);
-        res.status(201).json(data);
+        res.status(mapStatusHTTP(status)).json(data);
     }
 
     public update = async (req: Request, res: Response) => {
         const { id } = req.params;
         const { status, data } = await this.taskService.update(Number(id), req.body);
-        res.status(200).json(data);
+        res.status(mapStatusHTTP(status)).json(data);
     }
 
     public deleteTask = async (req: Request, res: Response) => {
         const { id } = req.params;
         const { status, data } = await this.taskService.deleteTask(Number(id));
-        res.status(200).json(data);
+        res.status(mapStatusHTTP(status)).json(data);
     }
 }
