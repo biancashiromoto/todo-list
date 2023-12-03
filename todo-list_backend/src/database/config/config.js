999987a8f9751207427a941ddd15a65b4e9a18dc
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-03 15:38:47 -0300

    raw: Remove unused lines

--- todo-list_backend/src/database/config/config.js
+++ todo-list_backend/src/database/config/config.js
@@ -2,15 +2,6 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// const config = {
-//   username: process.env.MYSQL_USER,
-//   password: process.env.MYSQL_PASSWORD,
-//   database: process.env.MYSQL_DATABASE,
-//   host: process.env.MYSQL_HOST,
-//   dialect: 'mysql',
-//   logging: false,
-// };
-
 const config = {
   username: process.env.MYSQL_USER || "root",
   password: process.env.MYSQL_PASSWORD || "root_password",
