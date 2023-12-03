1756928ae3bb2bff93bf6700b943a088de683f8e
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-03 18:29:17 -0300

    chore: Center tasks in task__item and display task status

--- todo-list_frontend/src/components/Task/Task.tsx
+++ todo-list_frontend/src/components/Task/Task.tsx
@@ -1,27 +1,28 @@
 import { useState } from "react";
 import { TaskType } from "../../types/TaskType";
+import "./Task.css";
 
 interface TaskProps {
     task: TaskType,
 }
 
 function Task({ task }: TaskProps) {
-  const [isTaskCompleted, setIsTaskCompleted] = useState<boolean>(task.status === "completed" ? true: false);
+  const [isTaskCompleted, setIsTaskCompleted] = useState<boolean>(task.status === "completed");
 
   return (
-    <div>
+    <div className="task__item">
       <label>
         <input
           type="checkbox"
           checked={isTaskCompleted}
           onChange={() => {
             setIsTaskCompleted(prevState => !prevState);
-            
           }}
         />
         {task.title}
       </label>
       <p>{task.priority}</p>
+      <p>{task.status}</p>
     </div>
   )
 }
