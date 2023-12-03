84551dd15dfb8283a4e23b650563640e231f5e72
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-03 15:39:45 -0300

    refactor: Change to OOP approach

--- todo-list_backend/src/controller/task.controller.ts
+++ todo-list_backend/src/controller/task.controller.ts
@@ -1,40 +1,38 @@
 import { Request, Response } from "express";
 import TaskService from "../services/task.service";
 
-const taskService: TaskService = new TaskService();
+export default class TaskController {
+    private taskService: TaskService;
 
-const findAll = async (req:Request, res: Response) => {
-    const { status, data } = await taskService.findAll();
-    res.status(200).json(data);
-}
+    constructor() {
+        this.taskService = new TaskService();
+    }
 
-const findByPk = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { status, data } = await taskService.findByPk(Number(id));
-    res.status(200).json(data);
-}
+    public findAll = async (req: Request, res: Response) => {
+        const { status, data } = await this.taskService.findAll();
+        res.status(200).json(data);
+    }
 
-const register = async (req: Request, res: Response) => {
-    const { status, data } = await taskService.register(req.body);
-    res.status(201).json(data);  
-}
+    public findByPk = async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const { status, data } = await this.taskService.findByPk(Number(id));
+        res.status(200).json(data);
+    }
 
-const update = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { status, data } = await taskService.update(Number(id), req.body);
-    res.status(200).json(data);
-}
+    public register = async (req: Request, res: Response) => {
+        const { status, data } = await this.taskService.register(req.body);
+        res.status(201).json(data);
+    }
 
-const deleteTask = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { status, data } = await taskService.deleteTask(Number(id));
-    res.status(200).json(data);
-}
+    public update = async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const { status, data } = await this.taskService.update(Number(id), req.body);
+        res.status(200).json(data);
+    }
 
-export default {
-    findAll,
-    findByPk,
-    register,
-    update,
-    deleteTask
+    public deleteTask = async (req: Request, res: Response) => {
+        const { id } = req.params;
+        const { status, data } = await this.taskService.deleteTask(Number(id));
+        res.status(200).json(data);
+    }
 }
