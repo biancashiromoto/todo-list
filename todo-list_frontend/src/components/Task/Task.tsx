e0f42807234fb0ec8478c65cee4418e649e83d4b
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-04 20:42:46 -0300

    feat: Create function changeStatus

--- todo-list_frontend/src/components/Task/Task.tsx
+++ todo-list_frontend/src/components/Task/Task.tsx
@@ -1,28 +1,46 @@
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { TaskType } from "../../types/TaskType";
 import "./Task.css";
+import Requests from "../../services/requests";
 
 interface TaskProps {
     task: TaskType,
 }
 
+const requests = new Requests();
+
 function Task({ task }: TaskProps) {
   const [isTaskCompleted, setIsTaskCompleted] = useState<boolean>(task.status === "completed");
+
+  const changeStatus = async (event: ChangeEvent<HTMLInputElement>, task: TaskType) => {
+    const { checked } = event.target;
+    
+    const updatedTask = {
+      ...task,
+      status: checked ? "completed" : "pending",
+    }
+    try {
+      task = await requests.update(updatedTask);
+      setIsTaskCompleted(task.status === "completed");
+      console.log("status: ", task.status);
+    } catch (error) {
+      console.error("Error updating task: ", error);
+    }
+  }
 
   return (
     <div className="task__item">
       <label>
         <input
+          defaultChecked={isTaskCompleted}
           type="checkbox"
           checked={isTaskCompleted}
-          onChange={() => {
-            setIsTaskCompleted(prevState => !prevState);
-          }}
+          onChange={(e) => changeStatus(e, task)}
         />
         {task.title}
       </label>
       <p>{task.priority}</p>
-      <p>{task.status}</p>
+      <p>{isTaskCompleted ? "completed" : "pending"}</p>
     </div>
   )
 }
