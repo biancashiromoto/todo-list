b777064126156634616109d213950c0bb74066f8
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-04 21:42:35 -0300

    feat: Implement task title update feature

--- todo-list_frontend/src/components/Task/Task.tsx
+++ todo-list_frontend/src/components/Task/Task.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { TaskType } from "../../types/TaskType";
 import "./Task.css";
 import Requests from "../../services/requests";
@@ -12,35 +12,55 @@ const requests = new Requests();
 function Task({ task }: TaskProps) {
   const [isTaskCompleted, setIsTaskCompleted] = useState<boolean>(task.status === "completed");
 
-  const changeStatus = async (event: ChangeEvent<HTMLInputElement>, task: TaskType) => {
-    const { checked } = event.target;
-    
-    const updatedTask = {
-      ...task,
-      status: checked ? "completed" : "pending",
-    }
-    try {
-      task = await requests.update(updatedTask);
-      setIsTaskCompleted(task.status === "completed");
-      console.log("status: ", task.status);
-    } catch (error) {
-      console.error("Error updating task: ", error);
+  const handleChange = async (e: ChangeEvent<HTMLInputElement> | FormEvent<HTMLParagraphElement>, task: TaskType) => {
+    if ("checked" in e.target) {
+      const { checked } = e.target;
+
+      const updatedTask = {
+        ...task,
+        status: checked ? "completed" : "pending",
+      }
+
+      try {
+        task = await requests.update(updatedTask);
+        setIsTaskCompleted(task.status === "completed");
+        console.log("status: ", task.status);
+      } catch (error) {
+        console.error("Error updating task: ", error);
+      }
+    } else {
+      const { innerText } = e.target as HTMLDivElement;
+      const updatedTask = {
+        ...task,
+        title: innerText,
+      }
+      try {
+        task = await requests.update(updatedTask);
+        console.log(task.title);
+      } catch (error) {
+        console.error("Error updating task: ", error);
+      }
     }
   }
 
   return (
     <div className="task__item">
-      <label>
         <input
-          defaultChecked={isTaskCompleted}
+          title="status"
           type="checkbox"
           checked={isTaskCompleted}
-          onChange={(e) => changeStatus(e, task)}
-        />
-        {task.title}
-      </label>
-      <p>{task.priority}</p>
-      <p>{isTaskCompleted ? "completed" : "pending"}</p>
+          onChange={(e) => handleChange(e, task)}
+          name="status"
+          />
+        <p
+          contentEditable={true}
+          onInput={(e) => handleChange(e, task)}
+          suppressContentEditableWarning={true}
+        >
+          {task.title}
+        </p>
+        <p>{task.priority}</p>
+        <p>{isTaskCompleted ? "completed" : "pending"}</p>
     </div>
   )
 }
