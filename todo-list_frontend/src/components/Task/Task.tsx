c94102dad76a11aa0785a3cea9e310a94c38be2e
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-05 16:13:04 -0300

    chore: Remove unused lines

--- todo-list_frontend/src/components/Task/Task.tsx
+++ todo-list_frontend/src/components/Task/Task.tsx
@@ -1,32 +1,18 @@
 import { useState } from "react";
 import { TaskType } from "../../types/TaskType";
 import "./Task.css";
-// import Requests from "../../services/requests";
 import Utils from "../../utils/Utils";
 
 interface TaskProps {
     task: TaskType,
 }
 
-// const requests = new Requests();
 const utils = new Utils();
 
 function Task({ task }: TaskProps) {
   const [isTaskCompleted, setIsTaskCompleted] = useState<boolean>(task.status === "completed");
   const [priority, setPriority] = useState<TaskType["priority"]>(task.priority);
   const [title, setTitle] = useState<TaskType["title"]>(task.title);
-
-  // const handleChange = async (e: ChangeEvent<HTMLInputElement | HTMLSelectElement> | FormEvent<HTMLParagraphElement>, task: TaskType) => {
-  //     const updatedTask = {
-  //       ...task,
-  //       title: e.currentTarget.textContent || "",
-  //     }
-  //     try {
-  //       task = await requests.update(updatedTask);
-  //     } catch (error) {
-  //       console.error("Error updating task: ", error);
-  //     }
-  //   }
 
   return (
     <div className="task__item">
