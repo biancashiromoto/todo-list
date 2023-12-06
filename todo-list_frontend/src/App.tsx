4fcd91febd1e5d128162431f46568b6fae83233b
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-06 20:40:43 -0300

    feat: Set setData as Task component prop

--- todo-list_frontend/src/App.tsx
+++ todo-list_frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { TaskType } from "./types/TaskType";
-import { mockTasks } from "./mocks/tasks.mock";
+// import { mockTasks } from "./mocks/tasks.mock";
 import Task from "./components/Task/Task";
 import Form from "./components/Form/Form";
 import Requests from "./services/requests";
@@ -20,7 +20,7 @@ function App() {
         console.error("ERROR: ", error);
       }
     }
-    setData(mockTasks);
+    // setData(mockTasks);
     fetchData();
   }, []);
 
@@ -30,11 +30,11 @@ function App() {
       <article className="task__list">
         {data.length > 0 ? (
           data.map((task: TaskType) => (
-            <Task key={task.id} task={task}/>
+            <Task key={task.id} task={task} setData={setData}/>
           ))
         ) : "No tasks."}
       </article>
-      <Form />
+      <Form data={data} setData={setData} />
     </>
   )
 }
