cc876534e2ed547cbf14dbe94dad24613ab0144b
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-06 19:54:26 -0300

    refactor: Implement currTask and setTask to manage task state and replace isTaskComplete and setIsTaskComplete

--- todo-list_frontend/src/components/Task/Task.tsx
+++ todo-list_frontend/src/components/Task/Task.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TaskType } from "../../types/TaskType";
 import "./Task.css";
 import Utils from "../../utils/Utils";
@@ -7,19 +7,29 @@ import { TaskProps } from "../../interfaces/TaskProp";
 const utils = new Utils();
 
 function Task({ task }: TaskProps) {
-  const [isTaskCompleted, setIsTaskCompleted] = useState<boolean>(task.status === "completed");
+  // const [isTaskCompleted, setIsTaskCompleted] = useState<boolean>(task.status === "completed");
   const [priority, setPriority] = useState<TaskType["priority"]>(task.priority);
   const [title, setTitle] = useState<TaskType["title"]>(task.title);
+
+  const [currTask, setCurrTask] = useState<TaskType>({
+    id: task.id,
+    title: task.title,
+    priority: task.priority,
+    status: task.status,
+  });
+
+  useEffect(() => {
+    console.log(currTask);
+  }, [currTask])
 
   return (
     <div className="task__item">
         <input
           title="status"
           type="checkbox"
-          checked={isTaskCompleted}
+          checked={currTask.status === "completed"}
           onChange={(e) => {
-            utils.changeStatus(e, task, setIsTaskCompleted);
-            setIsTaskCompleted(task.status === "completed");
+            utils.changeStatus(e, task, setCurrTask);
           }}
           name="status"
           />
@@ -44,7 +54,7 @@ function Task({ task }: TaskProps) {
           <option value="medium">medium</option>
           <option value="low">low</option>
         </select>
-        <p>{isTaskCompleted ? "completed" : "pending"}</p>
+        <p>{currTask.status}</p>
         <button
           type="button"
           onClick={(e) => {
