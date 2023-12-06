15d9422c49bdec18461497baad4b16cddd1eaf44
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-06 19:34:02 -0300

    feat: Create method deleteTask

--- todo-list_frontend/src/utils/Utils.ts
+++ todo-list_frontend/src/utils/Utils.ts
@@ -2,11 +2,12 @@ import { ChangeEvent, Dispatch, SetStateAction } from "react";
 import Requests from "../services/requests";
 import { NewTaskType, TaskType } from "../types/TaskType";
 
+const requests = new Requests();
 export default class Utils {
-  private requests: Requests;
+  // private requests: Requests;
 
   constructor() {
-    this.requests = new Requests();
+    // this.requests = new Requests();
   }
 
   /**
@@ -25,7 +26,7 @@ export default class Utils {
       }
 
       try {
-        task = await this.requests.update(updatedTask);
+        task = await requests.update(updatedTask);
         setIsTaskCompleted(task.status === "completed");
       } catch (error) {
         console.error("Error updating task: ", error);
@@ -46,7 +47,7 @@ export default class Utils {
         priority: e.target.value,
       }
       try {
-        task = await this.requests.update(updatedTask);
+        task = await requests.update(updatedTask);
         setPriority(selectedPriority);
       } catch (error) {
         console.error("Error updating task: ", error);
@@ -72,7 +73,7 @@ export default class Utils {
     };
   
     try {
-      task = await this.requests.update(updatedTask);
+      task = await requests.update(updatedTask);
       setTitle(newTitle);
     } catch (error) {
       console.error("Error updating task: ", error);
@@ -84,8 +85,20 @@ export default class Utils {
    */
   public async createTask(task: NewTaskType,) {
     try {
-      const createdTask = await this.requests.register(task);
+      const createdTask = await requests.register(task);
       console.log(createdTask);
+    } catch (error) {
+      console.error("Error creating task: ", error);
+    }
+  }
+
+  /**
+   * deleteTask
+   */
+  public async deleteTask(id: TaskType["id"]) {
+    try {
+      const message = await requests.delete(id);
+      console.log(message);
     } catch (error) {
       console.error("Error creating task: ", error);
     }
