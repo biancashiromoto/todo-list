fb11199f4a78e16c1d903ef490a225750ef9ac13
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-06 21:38:46 -0300

    style: Wrap task title and priority in div and add classNames

--- todo-list_frontend/src/components/Form/Form.tsx
+++ todo-list_frontend/src/components/Form/Form.tsx
@@ -3,6 +3,7 @@ import { FormProps } from "../../interfaces/Props";
 import { NewTaskType } from "../../types/TaskType";
 import Utils from "../../utils/Utils";
 import "../../App.css";
+import "./Form.css";
 
 const utils = new Utils();
 
@@ -14,36 +15,43 @@ function Form({ data, setData }: FormProps) {
   });
 
   return (
-    <form>
-      <label htmlFor="task-title">
-        <input
-          title="task-title"
-          type="text"
-          value={newTask.title}
+    <form className="task__form">
+      <h5>Create new task:</h5>
+      <div className="task__title-priority">
+        <label htmlFor="task-title">
+          <input
+            title="task-title"
+            type="text"
+            value={newTask.title}
+            onChange={(e) => {
+              setNewTask({ ...newTask, title: e.target.value});
+            }}
+            className="input input__title input__new-task--title"
+          />
+        </label>
+        <select
+          title="priority"
+          value={newTask.priority}
           onChange={(e) => {
-            setNewTask({ ...newTask, title: e.target.value});
+            setNewTask({...newTask, priority: e.target.value});
           }}
-        />
-      </label>
-      <select
-        title="priority"
-        value={newTask.priority}
-        onChange={(e) => {
-          setNewTask({...newTask, priority: e.target.value});
-        }}
-        name="priority"
-      >
-        <option value="high">high</option>
-        <option value="medium">medium</option>
-        <option value="low">low</option>
-      </select>
+          name="priority"
+          className="input input__priority"
+        >
+          <option value="high">high</option>
+          <option value="medium">medium</option>
+          <option value="low">low</option>
+        </select>
+      </div>
       <button
+        title="Create task"
         type="button"
         onClick={async (e) => {
           e.preventDefault();
           const createdTask = await utils.createTask(newTask, setNewTask);
           setData([...data, createdTask]);
         }}
+        className="button__create-task"
       >
         Create task
       </button>
