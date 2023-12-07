74ddba711d634cb96d1d0a02c1c731de828b78c4
Bianca Shiromoto <b.shiromoto.bs@gmail.com>
2023-12-07 12:59:23 -0300

    refactor: Change methods responses types

--- todo-list_backend/src/models/task.model.ts
+++ todo-list_backend/src/models/task.model.ts
@@ -1,35 +1,37 @@
 import { ITask } from "../interfaces/ITask";
 import { SequelizeTask } from "../database/models/sequelize.task.model"
-import { TaskType } from "../types/TaskType";
+import { TaskType, NewTaskType } from "../types/TaskType";
 
 export default class TaskModel implements ITask {
     private model = SequelizeTask;
     
-    async findAll(): Promise<ITask[]> {
+    async findAll(): Promise<TaskType[]> {
         const tasks = await this.model.findAll();
         return tasks.map((task) => task.dataValues);
     }
 
-    async findByPk(id: TaskType["id"]): Promise<ITask | null> {
+    async findByPk(id: TaskType["id"]): Promise<TaskType | null> {
         const task = await this.model.findByPk(id);
         if (!task) return null;
         return task.dataValues;
     }    
     
-    async register(task: TaskType): Promise<ITask> {
+    async register(task: TaskType): Promise<NewTaskType> {
+        const tasks = await this.model.findAll();
         const newTask = await this.model.create(task);
         return newTask.dataValues;
     }
     
-    async update(id: TaskType["id"], task: TaskType): Promise<ITask | null> {
+    async update(id: TaskType["id"], task: NewTaskType): Promise<TaskType | null> {
         const updatedLines = await this.model.update(task, { where: { id }});
         if (updatedLines[0] === 0) return null;
         const updatedTask = await this.model.findByPk(id);
         return updatedTask?.dataValues;
     }
 
-    async deleteTask(id: TaskType["id"]): Promise<string> {
+    async deleteTask(id: TaskType["id"]): Promise<TaskType[]> {
         await this.model.destroy({ where: { id }});
-        return "Task successfully deleted."
+        const tasks = await this.model.findAll();
+        return tasks.map((task) => task.dataValues);
     }
 }
